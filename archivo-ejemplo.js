81480d1bedcf9f61fd100b132773ed1b26f191fa
ArmandoEs <105843793+ArmandoEs@users.noreply.github.com>
2022-11-04 23:21:14 -0600

    Update archivo-ejemplo.js

---
+++ archivo-ejemplo.js
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charse="utf-8>
+<title>Documentosin titulo/title>
+<link href=css/estilos.css" rel="stylesheet" type"text/css">
+</head>
+
+<body>
+<h1>Proyecto empresarial</h1>
+<p>Parrafo agregado</p>
+</body>
+</html>
